f20232faf9a61e78b0bee9384f0d1a051d172877
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-12-08 16:48:55 +0500

    second array

--- task/Program.cs
+++ task/Program.cs
@@ -1,9 +1,17 @@
 ﻿//Console.Write("Введите количество элементов массива: ");
 //int size = int.Parse(Console.ReadLine());
 
-string[] array = {"hello", "2", "world", ":-)"}; //new string[size];
+string[] array = {"hello", "2", "world", ":-)"};
+string[] newArray = new string[array.Length];
+int i = 0;
 foreach (string el in array)
 {
-    Console.Write(el + " ");    
+    //Console.WriteLine(el + " "+ el.Length);  
+    if (el.Length < 4) 
+    {
+        //Console.WriteLine(el);
+        newArray[i] = el;
+        i++;
+    }
 }
 
