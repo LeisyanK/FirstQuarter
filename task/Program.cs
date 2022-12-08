556e06214b7c656c9d322dc3d9b0ddc0aedfe60a
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-12-08 19:37:02 +0500

    method NewArray

--- task/Program.cs
+++ task/Program.cs
@@ -4,11 +4,20 @@ void Main()
 {
     Console.Clear();
     string[] array = { "hello", "2", "world", ":-)" };
-    //string[] newArray = new string[array.Length];
-    string[] newArray = {};
+    string[] secondArray = {};
 
     Console.WriteLine("Исходный массив:");
     PrintArray(array);
+
+    secondArray = NewArray(array, secondArray);
+
+    Console.WriteLine("Новый массив:");
+    PrintArray(secondArray);
+    
+}
+
+string[] NewArray(string[] array, string[] newArray)
+{
     int i = 0;
     foreach (string el in array)
     {
@@ -19,20 +28,18 @@ void Main()
             i++;
         }
     }
-    Console.WriteLine();
-
-    Console.WriteLine("Новый массив:");
-    PrintArray(newArray);
-    
+    return newArray;
 }
 
 void PrintArray(string[] array)
 {
+    int i = 0;
     foreach (string el in array)
     {
-        //Console.WriteLine(el + " "+ el.Length);
         Console.Write(el + " ");
+        i++;
     }
+    Console.WriteLine();
 }
 
 void Resize(ref string[] array, int newSize)
